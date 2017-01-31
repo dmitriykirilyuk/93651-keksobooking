3062b595fa0996cd3c1925cd7fb34a5cf81dc9f9
dmitriykirilyuk <dimasishere@gmail.com>
2017-01-31 03:06:56 +0200

    Начал писать скрипт по форме

---
+++ js/form.js
@@ -0,0 +1,24 @@
+/*задание 1*/
+// var pin = document.querySelectorAll(".pin");
+//
+// pin.addEventListener("click", function() {
+//   pin.classList.add("pin--active");
+//
+//   if(pin--active = true) {
+//     this.classList.remove("pin--active");
+//   };
+// });
+
+/*задание 2*/
+
+/*задание 3*/
+var time = document.querySelectorAll("#time option");
+var timeOut = document.querySelectorAll("#timeout option");
+
+  if (time.value === 12) {
+    timeOut.value ===12;
+  } else if (time.value === 13) {
+    timeOut.value === 13;
+  } else {
+    timeOut.value === 14;
+  };

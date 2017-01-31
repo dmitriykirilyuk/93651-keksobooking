a903e6f26de0917b4fe3d9ba3d99790c2673d51f
dmitriykirilyuk <dimasishere@gmail.com>
2017-02-01 00:40:43 +0200

    Написал все, кроме 4го пункта

--- js/form.js
+++ js/form.js
@@ -1,24 +1,35 @@
+"use strict";
 /*задание 1*/
-// var pin = document.querySelectorAll(".pin");
-//
-// pin.addEventListener("click", function() {
-//   pin.classList.add("pin--active");
-//
-//   if(pin--active = true) {
-//     this.classList.remove("pin--active");
-//   };
-// });
+var pin = document.querySelectorAll(".pin");
+var i;
 
+for(i = 0; i < pin.length; i++) {
+  pin[i].addEventListener("click", function() {
+    if(this.classList.contains("pin--active")) {
+      this.classList.remove("pin--active");
+    } else {
+      this.classList.add("pin--active");
+    };
+  });
+};
 /*задание 2*/
+var dialogClose = document.querySelector(".dialog__close");
+var dialogPanel = document.querySelector(".dialog");
+var pin = document.querySelectorAll(".pin");/*почему если не обьявить переменную во второй раз, то ее не видит if ?*/
+dialogClose.addEventListener("click", function() {
+  dialogPanel.style.display = "none";
 
-/*задание 3*/
+  if(pin.classList.contains("pin--active")) {
+    this.classList.remove("pin--active");
+  };
+});
+
+/*задание 4*/
 var time = document.querySelectorAll("#time option");
 var timeOut = document.querySelectorAll("#timeout option");
 
-  if (time.value === 12) {
-    timeOut.value ===12;
-  } else if (time.value === 13) {
-    timeOut.value === 13;
-  } else {
-    timeOut.value === 14;
-  };
+for (var i = 0; i < time.length; i++) {
+  time[i].addEventListener("change", function () {
+    this.value == timeout.value;
+  });
+};
